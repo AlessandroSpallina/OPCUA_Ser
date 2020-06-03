7c71dab38e13044b2a453391e79b2f428d784e99
MASSIMO GOLLO <uni357307@studium.unict.it>
2020-06-03 23:12:17 +0200

    Fixed namespaces

--- OPC_UA_Server/OPC_UA_Server/test.c
+++ OPC_UA_Server/OPC_UA_Server/test.c
@@ -1,116 +1,116 @@
-#include <open62541/plugin/log_stdout.h>
-#include <open62541/server.h>
-#include <open62541/server_config_default.h>
-
-#include <signal.h>
-
-/* predefined identifier for later use */
-UA_NodeId cityTypeId = { 1, UA_NODEIDTYPE_NUMERIC, {1001} };
-
-static void
-defineObjectTypes(UA_Server* server) {
-    /* Define the object type for "Device" */
-    UA_NodeId deviceTypeId; /* get the nodeid assigned by the server */
-    UA_ObjectTypeAttributes dtAttr = UA_ObjectTypeAttributes_default;
-    dtAttr.displayName = UA_LOCALIZEDTEXT("en-US", "CityType");
-    UA_Server_addObjectTypeNode(server, UA_NODEID_NULL,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEOBJECTTYPE),
-        UA_NODEID_NUMERIC(0, UA_NS0ID_HASSUBTYPE),
-        UA_QUALIFIEDNAME(1, "CityType"), dtAttr,
-        NULL, &deviceTypeId);
-
-    UA_VariableAttributes mnAttr = UA_VariableAttributes_default;
-    mnAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Name");
-    UA_NodeId manufacturerNameId;
-    UA_Server_addVariableNode(server, UA_NODEID_NULL, deviceTypeId,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
-        UA_QUALIFIEDNAME(1, "Name"),
-        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), mnAttr, NULL, &manufacturerNameId);
-    /* Make the manufacturer name mandatory */
-    UA_Server_addReference(server, manufacturerNameId,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
-        UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true);
-
-
-    UA_VariableAttributes modelAttr = UA_VariableAttributes_default;
-    modelAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Latitude");
-    UA_Server_addVariableNode(server, UA_NODEID_NULL, deviceTypeId,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
-        UA_QUALIFIEDNAME(1, "Latitude"),
-        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), modelAttr, NULL, NULL);
-
-    /* Define the object type for "Pump" */
-    UA_ObjectTypeAttributes ptAttr = UA_ObjectTypeAttributes_default;
-    UA_NodeId weatherTypeId;
-    ptAttr.displayName = UA_LOCALIZEDTEXT("en-US", "WeatherType");
-    UA_Server_addObjectTypeNode(server, UA_NODEID_NULL,
-        //deviceTypeId, UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
-        deviceTypeId, UA_NODEID_NUMERIC(0, UA_NS0ID_HASSUBTYPE),
-        UA_QUALIFIEDNAME(1, "WeatherType"), ptAttr,
-        NULL, &weatherTypeId);
-
-    UA_VariableAttributes statusAttr = UA_VariableAttributes_default;
-    statusAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Temperature");
-    statusAttr.valueRank = UA_VALUERANK_SCALAR;
-    UA_NodeId statusId;
-    UA_Server_addVariableNode(server, UA_NODEID_NULL, weatherTypeId,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
-        UA_QUALIFIEDNAME(1, "Temperature"),
-        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), statusAttr, NULL, &statusId);
-    /* Make the status variable mandatory */
-    UA_Server_addReference(server, statusId,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
-        UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true);
-
-    UA_VariableAttributes rpmAttr = UA_VariableAttributes_default;
-    rpmAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Humidity");
-    rpmAttr.valueRank = UA_VALUERANK_SCALAR;
-    UA_Server_addVariableNode(server, UA_NODEID_NULL, weatherTypeId,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
-        UA_QUALIFIEDNAME(1, "Humidity"),
-        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), rpmAttr, NULL, NULL);
-}
-
-/**
- * Now we add the derived ObjectType for the pump that inherits from the device
- * object type. The resulting object contains all mandatory child variables.
- * These are simply copied over from the object type. The object has a reference
- * of type ``hasTypeDefinition`` to the object type, so that clients can detect
- * the type-instance relation at runtime.
- */
-
-static void
-addObjectInstance(UA_Server* server, char* name) {
-    UA_ObjectAttributes oAttr = UA_ObjectAttributes_default;
-    oAttr.displayName = UA_LOCALIZEDTEXT("en-US", name);
-    UA_Server_addObjectNode(server, UA_NODEID_NULL,
-        UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
-        UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
-        UA_QUALIFIEDNAME(1, name),
-        cityTypeId, /* this refers to the object type
-                       identifier */
-        oAttr, NULL, NULL);
-}
-
-static volatile UA_Boolean running = true;
-static void stopHandler(int sign) {
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "received ctrl-c");
-    running = false;
-}
-
-int main(void) {
-    signal(SIGINT, stopHandler);
-    signal(SIGTERM, stopHandler);
-
-    UA_Server* server = UA_Server_new();
-    UA_ServerConfig_setDefault(UA_Server_getConfig(server));
-
-    defineObjectTypes(server);
-    addObjectInstance(server, "Catania");
-    addObjectInstance(server, "Enna");
-
-    UA_StatusCode retval = UA_Server_run(server, &running);
-
-    UA_Server_delete(server);
-    return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
-}
+//#include <open62541/plugin/log_stdout.h>
+//#include <open62541/server.h>
+//#include <open62541/server_config_default.h>
+//
+//#include <signal.h>
+//
+///* predefined identifier for later use */
+//UA_NodeId cityTypeId = { 1, UA_NODEIDTYPE_NUMERIC, {1001} };
+//
+//static void
+//defineObjectTypes(UA_Server* server) {
+//    /* Define the object type for "Device" */
+//    UA_NodeId deviceTypeId; /* get the nodeid assigned by the server */
+//    UA_ObjectTypeAttributes dtAttr = UA_ObjectTypeAttributes_default;
+//    dtAttr.displayName = UA_LOCALIZEDTEXT("en-US", "CityType");
+//    UA_Server_addObjectTypeNode(server, UA_NODEID_NULL,
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEOBJECTTYPE),
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_HASSUBTYPE),
+//        UA_QUALIFIEDNAME(1, "CityType"), dtAttr,
+//        NULL, &deviceTypeId);
+//
+//    UA_VariableAttributes mnAttr = UA_VariableAttributes_default;
+//    mnAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Name");
+//    UA_NodeId manufacturerNameId;
+//    UA_Server_addVariableNode(server, UA_NODEID_NULL, deviceTypeId,
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
+//        UA_QUALIFIEDNAME(1, "Name"),
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), mnAttr, NULL, &manufacturerNameId);
+//    /* Make the manufacturer name mandatory */
+//    UA_Server_addReference(server, manufacturerNameId,
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+//        UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true);
+//
+//
+//    UA_VariableAttributes modelAttr = UA_VariableAttributes_default;
+//    modelAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Latitude");
+//    UA_Server_addVariableNode(server, UA_NODEID_NULL, deviceTypeId,
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
+//        UA_QUALIFIEDNAME(1, "Latitude"),
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), modelAttr, NULL, NULL);
+//
+//    /* Define the object type for "Pump" */
+//    UA_ObjectTypeAttributes ptAttr = UA_ObjectTypeAttributes_default;
+//    UA_NodeId weatherTypeId;
+//    ptAttr.displayName = UA_LOCALIZEDTEXT("en-US", "WeatherType");
+//    UA_Server_addObjectTypeNode(server, UA_NODEID_NULL,
+//        //deviceTypeId, UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
+//        deviceTypeId, UA_NODEID_NUMERIC(0, UA_NS0ID_HASSUBTYPE),
+//        UA_QUALIFIEDNAME(1, "WeatherType"), ptAttr,
+//        NULL, &weatherTypeId);
+//
+//    UA_VariableAttributes statusAttr = UA_VariableAttributes_default;
+//    statusAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Temperature");
+//    statusAttr.valueRank = UA_VALUERANK_SCALAR;
+//    UA_NodeId statusId;
+//    UA_Server_addVariableNode(server, UA_NODEID_NULL, weatherTypeId,
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
+//        UA_QUALIFIEDNAME(1, "Temperature"),
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), statusAttr, NULL, &statusId);
+//    /* Make the status variable mandatory */
+//    UA_Server_addReference(server, statusId,
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_HASMODELLINGRULE),
+//        UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY), true);
+//
+//    UA_VariableAttributes rpmAttr = UA_VariableAttributes_default;
+//    rpmAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Humidity");
+//    rpmAttr.valueRank = UA_VALUERANK_SCALAR;
+//    UA_Server_addVariableNode(server, UA_NODEID_NULL, weatherTypeId,
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
+//        UA_QUALIFIEDNAME(1, "Humidity"),
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), rpmAttr, NULL, NULL);
+//}
+//
+///**
+// * Now we add the derived ObjectType for the pump that inherits from the device
+// * object type. The resulting object contains all mandatory child variables.
+// * These are simply copied over from the object type. The object has a reference
+// * of type ``hasTypeDefinition`` to the object type, so that clients can detect
+// * the type-instance relation at runtime.
+// */
+//
+//static void
+//addObjectInstance(UA_Server* server, char* name) {
+//    UA_ObjectAttributes oAttr = UA_ObjectAttributes_default;
+//    oAttr.displayName = UA_LOCALIZEDTEXT("en-US", name);
+//    UA_Server_addObjectNode(server, UA_NODEID_NULL,
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+//        UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
+//        UA_QUALIFIEDNAME(1, name),
+//        cityTypeId, /* this refers to the object type
+//                       identifier */
+//        oAttr, NULL, NULL);
+//}
+//
+//static volatile UA_Boolean running = true;
+//static void stopHandler(int sign) {
+//    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "received ctrl-c");
+//    running = false;
+//}
+//
+//int main(void) {
+//    signal(SIGINT, stopHandler);
+//    signal(SIGTERM, stopHandler);
+//
+//    UA_Server* server = UA_Server_new();
+//    UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+//
+//    defineObjectTypes(server);
+//    addObjectInstance(server, "Catania");
+//    addObjectInstance(server, "Enna");
+//
+//    UA_StatusCode retval = UA_Server_run(server, &running);
+//
+//    UA_Server_delete(server);
+//    return retval == UA_STATUSCODE_GOOD ? EXIT_SUCCESS : EXIT_FAILURE;
+//}
