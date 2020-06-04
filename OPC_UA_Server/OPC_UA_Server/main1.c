44f30b253bada9b313fd6cabe40293061c64a1a4
MASSIMO GOLLO <uni357307@studium.unict.it>
2020-06-04 19:14:32 +0200

    added repeated job

--- OPC_UA_Server/OPC_UA_Server/main1.c
+++ OPC_UA_Server/OPC_UA_Server/main1.c
@@ -5,6 +5,17 @@
 #include <stdio.h>
 
 static void
+updateValueCallback(UA_Server* server, void* data) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "updateValueCallback");
+    UA_Float now = (UA_Float)rand() / 10; 
+    UA_Variant value;
+    UA_Variant_setScalar(&value, &now, &UA_TYPES[UA_TYPES_FLOAT]);
+    UA_NodeId currentNodeId = UA_NODEID_STRING(1, "weather-Catania-tmp");
+    UA_Server_writeValue(server, currentNodeId, value);
+
+}
+
+static void
 defineWeatherObject(UA_Server* server,char *locatioName) {
 
     /* get the nodeid assigned by the server */
@@ -17,8 +28,9 @@ defineWeatherObject(UA_Server* server,char *locatioName) {
         UA_QUALIFIEDNAME(1, "weatherQualifiedName"), UA_NODEID_NUMERIC(0, UA_NS0ID_BASEOBJECTTYPE),
         oAttr, NULL, &weatherId);
     //Primo attributo - Nome della località meteo
+
     UA_VariableAttributes mnAttr = UA_VariableAttributes_default;
-    UA_String cityName = UA_STRING(locatioName);
+    UA_String cityName = UA_STRING(locatioName);    
     UA_Variant_setScalar(&mnAttr.value, &cityName, &UA_TYPES[UA_TYPES_STRING]);
     mnAttr.displayName = UA_LOCALIZEDTEXT("en-US", "City Name");
     UA_Server_addVariableNode(server, UA_NODEID_NULL, weatherId,
@@ -26,11 +38,15 @@ defineWeatherObject(UA_Server* server,char *locatioName) {
         UA_QUALIFIEDNAME(1, "CityNameQualified"),
         UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), mnAttr, NULL, NULL);
     //Secondo attributo - Temperatura della città
+
     UA_VariableAttributes tmpAttr = UA_VariableAttributes_default;
+    char c[20];
+    sprintf(c, "weather-%s-tmp", locatioName); 
+    UA_NodeId attrNodeId = UA_NODEID_STRING(1, c);
     UA_Float Tmp = 0.0; 
     UA_Variant_setScalar(&tmpAttr.value, &Tmp, &UA_TYPES[UA_TYPES_FLOAT]);
     tmpAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Temperature");
-    UA_Server_addVariableNode(server, UA_NODEID_NULL, weatherId,
+    UA_Server_addVariableNode(server, attrNodeId, weatherId,
         UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
         UA_QUALIFIEDNAME(1, "TempQUalifiedName"),
         UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), tmpAttr, NULL, NULL);
@@ -61,6 +77,8 @@ int main(void) {
     defineWeatherObject(server, "Catania"); 
     defineWeatherObject(server, "Enna"); 
     defineWeatherObject(server, "Monciuffi"); 
+
+    UA_Server_addRepeatedCallback(server, updateValueCallback, NULL, 2000, NULL);
 
     UA_StatusCode retval = UA_Server_run(server, &running);
 
